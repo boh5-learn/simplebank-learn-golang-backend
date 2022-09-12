65bf95c5aba028240f505491b6c595b02e7d7f62
boh5 <bohuang1994@gmail.com>
2022-09-12 16:41:26 +0800

    :sparkles: Implement account api by gin

---
+++ api/server.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	db "github.com/boh5-learn/simplebank-learn-golang-backend/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// add routes to router
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
