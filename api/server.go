5655e9998e3323909e35dd4d842551a60689fc68
boh5 <bohuang1994@gmail.com>
2022-09-13 20:07:13 +0800

    :sparkles: Add create transfer API using custom gin validator

--- api/server.go
+++ api/server.go
@@ -1,8 +1,12 @@
 package api
 
 import (
+	"log"
+
 	db "github.com/boh5-learn/simplebank-learn-golang-backend/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 // Server serves HTTP requests for our banking service
@@ -20,11 +24,20 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		err := v.RegisterValidation("currency", validCurrency)
+		if err != nil {
+			log.Fatal("register validation err:", err)
+		}
+	}
+
 	// add routes to router
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
